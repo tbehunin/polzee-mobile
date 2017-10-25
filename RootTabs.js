9fca44e2f63611140c371a46792fb4db4dbd5001
Todd Behunin <tbehunin@gmail.com>
2017-10-25 15:25:39 -0700

    Added tabs

--- RootTabs.js
+++ RootTabs.js
@@ -1,28 +1,45 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import { TabNavigator } from 'react-navigation'; // 1.0.0-beta.14
 import Ionicons from 'react-native-vector-icons/Ionicons'; // 4.4.2
-
-const HomeScreen = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Home Screen</Text>
-  </View>
-);
-
-const ProfileScreen = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Profile Screen</Text>
-  </View>
-);
+import FeedScreen from './FeedScreen';
+import ExploreScreen from './ExploreScreen';
+import CreateScreen from './CreateScreen';
+import ProfileScreen from './ProfileScreen';
 
 const RootTabs = TabNavigator({
   Home: {
-    screen: HomeScreen,
+    screen: FeedScreen,
     navigationOptions: {
       tabBarLabel: 'Home',
       tabBarIcon: ({ tintColor, focused }) => (
         <Ionicons
           name={focused ? 'ios-home' : 'ios-home-outline'}
+          size={26}
+          style={{ color: tintColor }}
+        />
+      ),
+    },
+  },
+  Explore: {
+    screen: ExploreScreen,
+    navigationOptions: {
+      tabBarLabel: 'Explore',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Ionicons
+          name={focused ? 'ios-search' : 'ios-search-outline'}
+          size={26}
+          style={{ color: tintColor }}
+        />
+      ),
+    },
+  },
+  Create: {
+    screen: CreateScreen,
+    navigationOptions: {
+      tabBarLabel: 'Create',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Ionicons
+          name={focused ? 'ios-add-circle' : 'ios-add-circle-outline'}
           size={26}
           style={{ color: tintColor }}
         />
