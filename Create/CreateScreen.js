79b2fe2310d0c14af9db51e75269ce33e1b48a85
Todd Behunin <tbehunin@gmail.com>
2017-10-26 13:24:49 -0700

    converted to class and added event handler

--- Create/CreateScreen.js
+++ Create/CreateScreen.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 
-const CreateScreen = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Create Screen</Text>
-  </View>
-);
+class CreateScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: ''
+    };
+
+    this.handleChangeText = this.handleChangeText.bind(this);
+  }
+
+  handleChangeText(text) {
+    this.setState({question: text});
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <TextInput placeholder="What do you want to ask?" onChangeText={this.handleChangeText} />
+        <Text>{this.state.question}</Text>
+      </View>);
+  }
+}
 
 export default CreateScreen;
