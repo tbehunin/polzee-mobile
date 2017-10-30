ab5afa07fa7a5ecdcfedd59c21cd649b7a624903
Todd Behunin <tbehunin@gmail.com>
2017-10-30 15:27:23 -0700

    added thunk and logging middlewhere

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {createLogger} from 'redux-logger';
 import reducer from './app/reducers';
-import RootTabs from './RootTabs';
+import AppContainer from './app/containers/AppContainer';
+
+const loggerMiddleware = createLogger({predicate: (getState, action) => __DEV__});
 
 function configureStore(initialState) {
   // middleware here..
-  const enhancer = compose();
+  const enhancer = compose(
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware,
+    ),
+  );
 
   return createStore(reducer, initialState, enhancer);
 }
@@ -17,7 +26,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <RootTabs />
+        <AppContainer />
       </Provider>);
     }
 }
