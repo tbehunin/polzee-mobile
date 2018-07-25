b34ccd5fade9a8ba6bd71e570b7ea1f426103597
Todd Behunin <tbehunin@gmail.com>
2018-07-25 08:31:28 -0700

    new pool and client

--- app/actions/cognito/amplifyConfig.js
+++ app/actions/cognito/amplifyConfig.js
@@ -3,13 +3,13 @@ import Amplify from 'aws-amplify';
 Amplify.configure({
     Auth: {
         // REQUIRED - Amazon Cognito Identity Pool ID
-        identityPoolId: 'us-east-1:42a0e2b3-b98b-42d9-87b6-c50cb729a194',
+        identityPoolId: 'us-east-1:1ca28176-0c00-4ea7-949c-cf9d7737bab8',
         // REQUIRED - Amazon Cognito Region
         region: 'us-east-1',
         // OPTIONAL - Amazon Cognito User Pool ID
-        userPoolId: 'us-east-1_SLfdIUOlq',
+        userPoolId: 'us-east-1_6kPUJ8JtC',
         // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-        userPoolWebClientId: '3nph61apd5pj506gf14dfpemc2',
+        userPoolWebClientId: '1jlp5nu9tilkcm0fqe418bukpq',
         // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
         mandatorySignIn: false,
         // OPTIONAL - Configuration for cookie storage
